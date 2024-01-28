a8b71795d9edb1b81224566b47d4bb24647d52e6
Danila <maevepro28@mail.ru>
2024-01-28 03:39:17 +0300

    add funcs

--- funcs.py
+++ funcs.py
@@ -1,11 +1,11 @@
-def add_two_numbers(x, y):
+def add_two_numbers():
     x = int(input("Введите первое число: "))
     y = int(input("Введите второе число: "))
     return (f"Результат сложения: {x + y}")
 
 
 
-def subtract_numbers(x, y):
+def subtract_numbers():
     x = int(input("Введите первое число: "))
     y = int(input("Введите второе число: "))
 
---
+++ main.py
@@ -0,0 +1,3 @@
+from funcs import add_two_numbers
+
+add_numbers = add_two_numbers()
