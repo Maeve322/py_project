7ff46476f6db54d214b6399d899764c2f619a94f
Danila <maevepro28@mail.ru>
2024-01-28 03:28:01 +0300

    add funcs add_two_numbers

---
+++ funcs.py
@@ -0,0 +1,8 @@
+def add_two_numbers(x, y):
+    return x + y
+
+x = int(input("Введите первое число: "))
+y = int(input("Введите второе число: "))
+
+result = add_two_numbers(x, y)
+print("Результат сложения:", result)
